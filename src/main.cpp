68bbac780d523bb2fe70f5046431c695b42dadcc
topkek <pierre.fichepoil@intradef.gouv.fr>
2018-06-09 23:42:34 +0200

    rendergraph + sublimelinter project

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,7 @@
 #include "Window.hpp"
 #include <iomanip>
 #include <unistd.h>
-
+#include <vector>
 struct	t_sdl
 {
 	SDL_Window		*win;
@@ -39,6 +39,72 @@ void	terminal(void)
 	}
 }
 
+
+
+
+double remap(double value, double low1, double high1, double low2, double high2)
+{
+	return (low2 + (value - low1) * (high2 - low2) / (high1 - low1));
+}
+
+
+
+void renderGraph(int x, int y, int w, int h)
+{
+
+	SDL_Rect	r;
+
+	r.x = x;
+	r.y = y;
+	r.w = w;
+	r.h = h;
+
+
+	std::vector<int> cpuHistoryGraph;
+	cpuHistoryGraph.reserve(100);
+	while (cpuHistoryGraph.size() != cpuHistoryGraph.capacity())
+			cpuHistoryGraph.push_back(rand()%101);
+	
+	int currentPercantage = cpuHistoryGraph[cpuHistoryGraph.size()-1];
+
+	SDL_SetRenderDrawColor(s.renderer, 52, 62, 77, 255);
+	SDL_RenderFillRect(s.renderer, &r);
+
+	SDL_Rect	r2;
+	int offset = remap(static_cast<double>(100-currentPercantage), static_cast<double>(0), static_cast<double>(100), static_cast<double>(0), static_cast<double>(h) );
+	r2.x = x;
+	r2.y = y + offset;
+	r2.w = w;
+	r2.h = h  - offset;
+
+	SDL_SetRenderDrawColor(s.renderer, 47, 85, 101, 255);
+	int i0 = 50;
+	while (i0 < w)
+	{
+		SDL_RenderDrawLine(s.renderer, x + i0, y, x + i0, y+h);
+		i0 += 50;
+	}
+
+	SDL_SetRenderDrawColor(s.renderer, 57, 104, 123, 255);
+	SDL_RenderFillRect(s.renderer, &r2);
+
+	SDL_SetRenderDrawColor(s.renderer, 78, 142, 168, 255);
+	int i = 0;
+	SDL_Point list[ cpuHistoryGraph.size()];
+	while (i < cpuHistoryGraph.size())
+	{
+		double realx = remap(static_cast<double>(i), static_cast<double>(0), static_cast<double>(99), static_cast<double>(0), static_cast<double>(w) );
+		double realy = remap(static_cast<double>(100-cpuHistoryGraph[i]), static_cast<double>(0), static_cast<double>(100), static_cast<double>(0), static_cast<double>(h) );
+		list[i].x = x + realx;
+		list[i].y = y + realy;
+		i++;
+	}
+	SDL_RenderDrawLines(s.renderer, list, cpuHistoryGraph.size());
+
+
+	SDL_RenderPresent(s.renderer);
+}
+
 void	renderRect(void)
 {
 	static int	pos = 0;
@@ -63,6 +129,8 @@ void	windowed(void)
 	SDL_RenderClear(s.renderer);
 	SDL_RenderPresent(s.renderer);
 	renderRect();
+    SDL_SetRenderDrawColor(s.renderer, 255, 255, 255, SDL_ALPHA_OPAQUE);
+	renderGraph(500,500,1000,200);
 	SDL_Delay(5000);
 	SDL_DestroyWindow(s.win);
 	SDL_Quit();
